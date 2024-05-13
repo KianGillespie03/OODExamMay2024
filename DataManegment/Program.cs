920e1dc8a5c37ca5a6696126632dbbf4c5e96899
S00235539 <S00235539@LAB-D2038-31>
2024-05-13 12:58:45 +0100

    Add project files.

---
+++ DataManegment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataManegment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
